28dfdc3781275f860360169e638fc3f36b9d7e17
viktor-rak-laptop <vrakov2012@gmail.com>
2025-02-27 13:16:20 -0500

    added frequency map

--- main.go
+++ main.go
@@ -13,9 +13,14 @@ type Node struct {
 }
 
 func main() {
+	frequency := make(map[rune]int)
 	scanner := bufio.NewScanner(os.Stdin)
 	fmt.Printf("Enter a string you would like to encode: ")
 	scanner.Scan()
 	input := scanner.Text()
+	for _, freq := range input {
+		frequency[freq]++
+	}
 	fmt.Println("Input: ", input)
+	fmt.Println(frequency)
 }
